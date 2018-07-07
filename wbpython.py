4e281e28f42afd22074b1058e042d72f7fe11e39
Vittorio De Angelis <vittoriodeangelis@Vittorios-MacBook-Pro.local>
2018-07-07 13:16:27 +0800

    added python file and teplates folder

--- wbpython.py
+++ wbpython.py
@@ -1,13 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, world!!!!!"
-
-
-
+    # return "Hello, world!!!!!"
+    return render_template("page0.html")
+# @app.route("/david")
+# def david():
+#     return "Hello David"
+#
+# @app.route("/maria")
+# def maria():
+#     return "Hello Maria"
+#
+# @app.route("/<string:name>")
+# def hello(name):
+#     name = name.capitalize()
+#     return "<h1>Hello, {}<h1>".format(name)
 
 if __name__ == '__main__':
     app.run(debug=True)
